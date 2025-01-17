07e5b28c0950bcba4edb984227d9b4b1d62e45ff
Julien <julien.hanse12@gmail.com>
2025-01-17 18:38:04 +0100

    add: initialize test

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,25 +1,40 @@
 import { PasswordChecker } from "../src/passwordChecker.js";
 
-describe("Password validation", () => {
-	const fonction = new PasswordChecker();
+describe("Password validation - initialize", () => {
+	const main = new PasswordChecker();
 
-	it("should return false for passwords shorter than 8 characters", () => {
-		const result = fonction.isValid("1234567");
+	it("should return false for passwords is empty", () => {
+		const result = main.isValid("");
+
 	  	expect(result).toBe(false);
 	});
 
-	it("should return false for passwords without special characters", () => {
-		const result = fonction.isValid("abcdefgh");
-	  	expect(result).toBe(false);
+	it("should return false for passwords is null", () => {
+		const result = main.isValid(null);
+
+		expect(result).toBe(false);
 	});
 
-	it("should return false for passwords without numbers", () => {
-		const result = fonction.isValid("abcdefgh");
-	  	expect(result).toBe(false);
-	});
+	it("should return false for passwords is not string", () => {
+		const result = main.isValid(false);
 
-	it("should return false for passwords containing 'ipl'", () => {
-		const result = fonction.isValid("123ipl45");
-	  	expect(result).toBe(false);
+		expect(result).toBe(false);
 	});
 });
+
+/*
+it("should return false for passwords without special characters", () => {
+    const result = fonction.isValid("abcdefgh");
+      expect(result).toBe(false);
+});
+
+it("should return false for passwords without numbers", () => {
+    const result = fonction.isValid("abcdefgh");
+      expect(result).toBe(false);
+});
+
+it("should return false for passwords containing 'ipl'", () => {
+    const result = fonction.isValid("123ipl45");
+      expect(result).toBe(false);
+});
+ */
