67a3c59d83c89d995964c9b6dd4a66c371ef72bf
Julien <julien.hanse12@gmail.com>
2025-01-17 18:52:57 +0100

    add: all test with Jasmine

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,6 +1,6 @@
 import { PasswordChecker } from "../src/passwordChecker.js";
 
-describe("Password validation - initialize", () => {
+describe("Password validation - Initialize", () => {
 	const main = new PasswordChecker();
 
 	it("should return false for passwords is empty", () => {
@@ -22,19 +22,66 @@ describe("Password validation - initialize", () => {
 	});
 });
 
-/*
-it("should return false for passwords without special characters", () => {
-    const result = fonction.isValid("abcdefgh");
-      expect(result).toBe(false);
+describe("Password validation - Length", () => {
+	const main = new PasswordChecker();
+
+	it("should return false for passwords shorter than 8 characters", () => {
+		const result = main.isValid("abcdefgh");
+
+		expect(result).toBe(false);
+	});
+
+	it("should return true for passwords bigger or equals than 8 characters", () => {
+		const result = main.isValid("ab*c1def");
+
+		expect(result).toBe(true);
+	});
 });
 
-it("should return false for passwords without numbers", () => {
-    const result = fonction.isValid("abcdefgh");
-      expect(result).toBe(false);
+describe("Password validation - Special characters", () => {
+	const main = new PasswordChecker();
+
+	it("should return false for passwords without special characters", () => {
+		const result = main.isValid("abcdefgh");
+
+		expect(result).toBe(false);
+	});
+
+	it("should return true for passwords with one special character", () => {
+		const result = main.isValid("ab*c1def");
+
+		expect(result).toBe(true);
+	});
 });
 
-it("should return false for passwords containing 'ipl'", () => {
-    const result = fonction.isValid("123ipl45");
-      expect(result).toBe(false);
+describe("Password validation - Numbers", () => {
+	const main = new PasswordChecker();
+
+	it("should return false for passwords without numbers", () => {
+		const result = main.isValid("abcdefgh");
+
+		expect(result).toBe(false);
+	});
+
+	it("should return true for passwords with one number", () => {
+		const result = main.isValid("ab*c1def");
+
+		expect(result).toBe(true);
+	});
 });
- */
+
+describe("Password validation - Blacklist words", () => {
+	const main = new PasswordChecker();
+
+	it("should return false for passwords containing IPL", () => {
+		const result = main.isValid("ab*c1deIPLf");
+
+		expect(result).toBe(false);
+	});
+
+	it("should return true for passwords not containing IPL", () => {
+		const result = main.isValid("ab*c1def");
+
+		expect(result).toBe(true);
+	});
+});
